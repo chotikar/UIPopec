ef6046d78bbb63adffd1ca6ee6c2119bbf75e08c
Popp <Popp@Popp-Macbook.local>
2017-03-25 15:21:54 +0700

    Add contact view

---
+++ ContactViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ContactViewController.swift
+//  UIPSOPEC
+//
+//  Created by Popp on 3/25/17.
+//  Copyright © 2017 Senior Project. All rights reserved.
+//
+
+import UIKit
+import SWRevealViewController
+
+class ContactViewController : UIViewController {
+    
+    @IBOutlet weak var MenuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Sidemenu()
+        CustomNavbar()
+        
+    }
+    
+    func Sidemenu() {
+        if revealViewController() != nil {
+            MenuButton.target = SWRevealViewController()
+            MenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    func CustomNavbar() {
+        navigationController?.navigationBar.barTintColor = UIColor.red
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+    }
+
+    
+}

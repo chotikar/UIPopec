7e0fed91c574422c46bba8183ed3cc25d5857d3f
Popp <Popp@Popp-Macbook.local>
2017-03-27 21:41:21 +0700

    Add icon in Contact Page

--- ContactTableViewController.swift
+++ ContactTableViewController.swift
@@ -60,13 +60,15 @@ class ContactTableViewController : UITableViewController {
         cell.selectionStyle = .none
         let contactdetail = self.contactlist[indexPath.row]
         cell.campusName.text = contactdetail.CampusnameEn
-        cell.latitude.text = contactdetail.Latitude
-        cell.longtitude.text = contactdetail.Longtitude
         cell.addrEn.text = contactdetail.AddrEn
         cell.addrTh.text = contactdetail.AddrTh
-        cell.tel.text = contactdetail.Telephone
-        cell.fax.text = contactdetail.Fax
+        cell.tel.text = "Tel. \(contactdetail.Telephone)"
+        cell.fax.text = "Fax. \(contactdetail.Fax)"
         cell.email.text = contactdetail.Email
+        cell.addrlogo1.image = UIImage(named: "map")
+        cell.addrlogo2.image = UIImage(named: "map")
+        cell.tellogo.image = UIImage(named: "phone-call")
+        cell.emaillogo.image = UIImage(named: "mail")
         return cell
     }
     
@@ -79,8 +81,6 @@ class ContactCell : UITableViewCell {
     
 //    @IBOutlet var abaclogo : UIImageView!
     @IBOutlet var campusName : UILabel!
-    @IBOutlet var latitude : UILabel!
-    @IBOutlet var longtitude : UILabel!
     @IBOutlet var addrEn : UILabel!
     @IBOutlet var addrTh : UILabel!
     @IBOutlet var tel : UILabel!
@@ -88,21 +88,23 @@ class ContactCell : UITableViewCell {
     @IBOutlet var email : UILabel!
     @IBOutlet var addrlogo1 : UIImageView!
     @IBOutlet var addrlogo2 : UIImageView!
-//    @IBOutlet var tellogo : UIImageView!
-//    @IBOutlet var emaillogo : UIImageView!
+    @IBOutlet var tellogo : UIImageView!
+    @IBOutlet var emaillogo : UIImageView!
     
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 //        self.contentView.addSubview(abaclogo)
         self.contentView.addSubview(campusName)
-        self.contentView.addSubview(latitude)
-        self.contentView.addSubview(longtitude)
         self.contentView.addSubview(addrEn)
         self.contentView.addSubview(addrTh)
         self.contentView.addSubview(tel)
         self.contentView.addSubview(fax)
         self.contentView.addSubview(email)
+        self.contentView.addSubview(addrlogo1)
+        self.contentView.addSubview(addrlogo2)
+        self.contentView.addSubview(tellogo)
+        self.contentView.addSubview(emaillogo)
     
     }
     
@@ -119,31 +121,27 @@ class ContactCell : UITableViewCell {
 //        abaclogo.clipsToBounds = true
         
         campusName.font = UIFont.boldSystemFont(ofSize: 20)
-        campusName.frame = CGRect(x: 10, y: 10, width: 350, height: 30)
-        
-        latitude.font = UIFont.systemFont(ofSize: 12)
-        latitude.frame = CGRect(x: 10, y: 30, width: 100, height: 30)
-        latitude.textColor = UIColor.blue
-        
-        longtitude.font = UIFont.systemFont(ofSize: 12)
-        longtitude.frame = CGRect (x: 150, y: 30, width: 100, height: 30)
-        longtitude.textColor = UIColor.blue
+        campusName.frame = CGRect(x: 40, y: 10, width: scWid, height: 30)
         
         addrEn.font = UIFont.systemFont(ofSize: 12)
-        addrEn.frame = CGRect(x: 10, y: 50, width: 350, height: 30)
+        addrEn.frame = CGRect(x: 40, y: 40, width: scWid, height: 30)
 
         addrTh.font = UIFont.systemFont(ofSize: 12)
-        addrTh.frame = CGRect(x: 10, y: 70, width: 350, height: 30)
+        addrTh.frame = CGRect(x: 40, y: 60, width: scWid, height: 30)
         
         tel.font = UIFont.systemFont(ofSize: 12)
-        tel.frame = CGRect(x: 10, y: 90, width: 350, height: 30)
+        tel.frame = CGRect(x: 40, y: 80, width: scWid, height: 30)
         
         fax.font = UIFont.systemFont(ofSize: 12)
-        fax.frame = CGRect(x: 10, y: 110, width: 350, height: 30)
+        fax.frame = CGRect(x: 140, y: 80, width: scWid, height: 30)
     
         email.font = UIFont.systemFont(ofSize: 12)
-        email.frame = CGRect(x: 10, y: 130, width: 350, height: 30)
+        email.frame = CGRect(x: 40, y: 100, width: scWid, height: 30)
         
+        addrlogo1.frame = CGRect(x: 10, y: 45, width: scWid*0.05, height: scWid*0.05)
+        addrlogo2.frame = CGRect(x: 10, y: 65, width: scWid*0.05, height: scWid*0.05)
+        tellogo.frame = CGRect(x: 10, y: 85, width: scWid*0.05, height: scWid*0.05)
+        emaillogo.frame = CGRect(x: 10, y: 105, width: scWid*0.05, height: scWid*0.05)
     }
     
     
